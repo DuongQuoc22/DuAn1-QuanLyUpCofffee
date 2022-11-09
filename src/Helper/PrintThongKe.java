3d67b9c07d045cba45d7ac6a59cc117a6b58ae50
DuongQuoc2002 <duong.quoc304@gmail.com>
2022-11-09 15:43:21 +0700

    update helper

---
+++ src/Helper/PrintThongKe.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Helper;
+
+
+import DAO.BanChitietDAO;
+import DAO.HoaDonDAO;
+import Entity.Hoadon;
+import com.itextpdf.text.Chapter;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.FileOutputStream;
+import javax.swing.GroupLayout;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Admin
+ */
+public class PrintThongKe {
+    
+}
